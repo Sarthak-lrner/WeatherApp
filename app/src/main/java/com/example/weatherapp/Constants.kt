56576a8017449a53260543e95a9bb12e6c7ac3a0
Sarthak-lrner <sarthaksood204@gmail.com>
2026-07-27 22:15:59 +0530

    Completed Project

--- app/src/main/java/com/example/weatherapp/Constants.kt
+++ app/src/main/java/com/example/weatherapp/Constants.kt
@@ -10,6 +10,8 @@ object Constants {
     const val APP_ID = BuildConfig.WEATHER_API_KEY
     const val BASE_URL: String = "https://api.openweathermap.org/data/"
     const val METRIC_UNIT: String = "metric"
+    const val PREFERENCE_NAME = "weatherAppPreference"
+    const val WEATHER_RESPONSE_DATA = "weather_response_data"
     fun isNetworkAvailable(context: Context): Boolean{
         val connectivityManager = context
             .getSystemService(Context.CONNECTIVITY_SERVICE) as
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.app.Dialog
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
@@ -14,8 +15,11 @@ import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.models.WeatherResponse
 import com.example.weatherapp.network.WeatherService
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -23,6 +27,7 @@ import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.gson.Gson
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -33,18 +38,26 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mFusedLocationClient : FusedLocationProviderClient
     private var mProgressDialog: Dialog?= null
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mSharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
+        mSharedPreferences = getSharedPreferences(Constants.PREFERENCE_NAME, Context.MODE_PRIVATE)
+        setupUI()
         if(!isLocationEnabled()){
             Toast.makeText(this, "Your Location provider is turned off. Please turn on location",
                 Toast.LENGTH_SHORT).show()
@@ -134,12 +147,18 @@ class MainActivity : AppCompatActivity() {
 
             listCall.enqueue(object : Callback<WeatherResponse> {
                 override fun onResponse(
-                    call: Call<WeatherResponse>?,
-                    response: Response<WeatherResponse>?
+                    call: Call<WeatherResponse>,
+                    response: Response<WeatherResponse>
                 ) {
-                    if(response!!.isSuccessful){
+                    if(response.isSuccessful){
                         hideProgressDialog()
                         val weatherList: WeatherResponse? = response.body()
+                        val weatherResponseJsonString = Gson().toJson(weatherList)
+
+                        val editor = mSharedPreferences.edit()
+                        editor.putString(Constants.WEATHER_RESPONSE_DATA, weatherResponseJsonString)
+                        editor.apply()
+                        setupUI()
                         Log.i("Response Result123", "$weatherList")
                     }else{
                         val rc = response.code()
@@ -193,19 +212,90 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showCustomProgressDialogFunction() {
-        val customProgressDialog = Dialog(this)
+        mProgressDialog = Dialog(this)
 
-        /*Set the screen content from a layout resource.
-        The resource will be inflated, adding all top-level views to the screen.*/
-        customProgressDialog.setContentView(R.layout.dialog_custom_progress)
-
-        //Start the dialog and display it on screen.
-        customProgressDialog.show()
+        mProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+        mProgressDialog?.setCancelable(false)
+        mProgressDialog?.show()
     }
 
-    private fun hideProgressDialog(){
-        if(mProgressDialog != null){
-            mProgressDialog!!.dismiss()
+    private fun hideProgressDialog() {
+        mProgressDialog?.dismiss()
+        mProgressDialog = null
+    }
+
+    private fun setupUI() {
+        val weatherResponseJsonString = mSharedPreferences.getString(Constants.WEATHER_RESPONSE_DATA, "")
+
+        if(!weatherResponseJsonString.isNullOrEmpty()){
+            val weatherList = Gson().fromJson(weatherResponseJsonString, WeatherResponse::class.java)
+            for(i in weatherList.weather.indices){
+                Log.i("Weather Name", weatherList.weather.toString())
+
+                binding.tvMain.text = weatherList.weather[i].main
+                binding.tvMainDescription.text = weatherList.weather[i].description
+                binding.tvTemp.text = weatherList.main.temp.toString() + getUnit(application.resources.configuration.locales.toString())
+
+                binding.tvHumidity.text = weatherList.main.humidity.toString() + " per cent"
+                binding.tvMin.text = weatherList.main.temp_min.toString() + " min"
+                binding.tvMax.text = weatherList.main.temp_max.toString() + " max"
+                binding.tvSpeed.text = weatherList.wind.speed.toString()
+                binding.tvName.text = weatherList.name
+                binding.tvCountry.text = weatherList.sys.country
+
+                binding.tvSunriseTime.text =
+                    unixTime(weatherList.sys.sunrise, weatherList.timezone)
+                binding.tvSunsetTime.text =
+                    unixTime(weatherList.sys.sunset, weatherList.timezone)
+
+                when(weatherList.weather[i].icon){
+                    "01d" -> binding.ivMain.setImageResource(R.drawable.sunny)
+                    "02d" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                    "03d" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                    "04d" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                    "10d" -> binding.ivMain.setImageResource(R.drawable.rain)
+                    "11d" -> binding.ivMain.setImageResource(R.drawable.storm)
+                    "13d" -> binding.ivMain.setImageResource(R.drawable.snowflake)
+                    "01n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                    "02n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                    "03n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                    "10n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                    "11n" -> binding.ivMain.setImageResource(R.drawable.rain)
+                    "13n" -> binding.ivMain.setImageResource(R.drawable.snowflake)
+                }
+            }
+        }
+
+    }
+
+    private fun getUnit(value: String): String{
+        var value = "°C"
+        if("US" == value || "LR" == value || "MM" ==value){
+            value = "°F"
+        }
+        return value
+    }
+
+    private fun unixTime(timestamp: Long, timezoneOffset: Int): String {
+        val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
+        sdf.timeZone = TimeZone.getTimeZone("UTC")
+
+        val date = Date((timestamp + timezoneOffset) * 1000L)
+        return sdf.format(date)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_refresh -> {
+                requestLocationData()
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
